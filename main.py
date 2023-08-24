2b3e9c0c890f36a479cec23ff1c805a19aba819b
Muhammad Najmu Syathir <alsyathir@gmail.com>
2023-08-24 19:44:21 +0800

    Fixed import data from extension

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
+from router import cpu_GET
+from router import cpu_POST
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# 1. Implement venv >python -m venv fastapi-venv
+# 2. Activate the venv >C:\*\Scripts\activate.bat
+# 3. Install uvicorn >pip install uvicorn
+# 4. Install uvicorn >pip install fastapi
+# 5. Check installation success >uvicorn --version
+# 6. Running uvicorn >uvicorn main:app --reload
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from router import cpu_GET
 from router import cpu_POST
 from router import dataHandler
 
-# reload - uvicorn main:app --reload
 
 app = FastAPI()
+
+
+# Configure CORS to allow requests from your extension's origin
+origins = [
+
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include routers
 app.include_router(cpu_GET.router)
 app.include_router(cpu_POST.router)
 app.include_router(dataHandler.router)
 
-# basic function
-@app.get('/', tags=['Default'])
-def index():
-    return {'message': 'main page of PC Compatibility Checker'}
 
+# Default route
+@app.get("/", tags=["Default"])
+def index():
+    return {"message": "Main page of PC Compatibility Checker"}
